18b11f80f4f91cdaa65fa3ec5f9424ba233b5019
James Barwell <jb@jamesbarwell.co.uk>
2018-03-06 14:57:02 +0000

    Add Redis connector

---
+++ src/connection/RedisConnection.js
@@ -0,0 +1,74 @@
+const redis = require('redis');
+
+module.exports = (
+    logger,
+    metrics,
+    host,
+    port,
+    dbIndex,
+) => {
+    logger.info('redisconnector.init', {
+        host,
+        port,
+        dbIndex,
+    });
+
+    let client = null;
+
+    const RedisConnector = {
+        client: () => {
+            if (client) {
+                return client;
+            }
+
+            client = redis.createClient(
+                port,
+                host,
+                { connect_timeout: 2000, dbIndex },
+            );
+            client.on('error', (err) => {
+                logger.error(
+                    'stats-worker.cache.error',
+                    {
+                        message: err.toString(),
+                    },
+                );
+                client = null;
+            });
+
+            return client;
+        },
+    };
+
+    RedisConnector.set = (key, value) => {
+        logger.info('cache.set', {
+            key,
+        });
+
+        return new Promise((resolve, reject) => {
+            RedisConnector.client().set(key, value, (err, response) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(response);
+                }
+            });
+        });
+    };
+
+    RedisConnector.delete = (key) => {
+        logger.info('cache.delete', { key });
+
+        return new Promise((resolve, reject) => {
+            RedisConnector.client().del(key, (err, response) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(response);
+                }
+            });
+        });
+    };
+
+    return RedisConnector;
+};
