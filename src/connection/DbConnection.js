d0af4d630b9130068c635f6413af1e9f483f030b
David Jimenez Sequero <david.jimenez-sequero@infinityworks.com>
2018-05-08 13:22:17 +0100

    Add DB connection limit option, explain all overridden pool options

--- src/connection/DbConnection.js
+++ src/connection/DbConnection.js
@@ -3,9 +3,27 @@
 const mysql = require('mysql2');
 
 const DEFAULT_OUTPUT_LABEL = 'connector.DBConnection';
-const QUEUE_LIMIT = 100;
+// These flags are expected to be reasonably stable for any service
+// but are mainly performance-driven, so as to prevent connections from backing up
+// and services becoming silently overwhelmed and unresponsive.
+// Further documentation on these flags can be found at https://github.com/mysqljs/mysql#pool-options
+//
+// Connection pool size, i.e. number of connections that can be ready in the pool
+// They're lazily loaded
+const CONNECTION_LIMIT = 200;
+
+// Milliseconds to wait to be allocated a connection from the pool
 const ACQUIRE_TIMEOUT = 1000;
+
+// If set to `true`, the driver will queue up any newly requested connections if the amount
+// exceeds the current connection limit.
+// If `false`, the driver will immediately error out when it runs out of connections. This is to
+// prevent a backlog of connection requests from silently backing up.
 const WAIT_FOR_CONNECTIONS = false;
+
+// Only applicable if `waitForConnections` is true
+// Maximum amount of requests to push into the queue awaiting a connection from the pool
+const QUEUE_LIMIT = 100;
 
 module.exports = (
     logger,
@@ -23,6 +41,7 @@ module.exports = (
         user,
         password,
         database,
+        connectionLimit: CONNECTION_LIMIT,
         queueLimit: QUEUE_LIMIT,
         acquireTimeout: ACQUIRE_TIMEOUT,
         waitForConnections: WAIT_FOR_CONNECTIONS,
